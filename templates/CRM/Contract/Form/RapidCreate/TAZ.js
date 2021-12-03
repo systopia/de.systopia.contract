b835b9548f97234a15caec16c7b3d3a46b3423be
B. Endres <endres@systopia.de>
2021-12-03 11:48:06 +0100

    added TAZ domain

---
+++ templates/CRM/Contract/Form/RapidCreate/TAZ.js
@@ -0,0 +1,13 @@
+/*-------------------------------------------------------------+
+| SYSTOPIA Contract Extension                                  |
+| Copyright (C) 2017-2019 SYSTOPIA                             |
+| Author: B. Endres (endres -at- systopia.de)                  |
+|         M. McAndrew (michaelmcandrew@thirdsectordesign.org)  |
+|         P. Figel (pfigel -at- greenpeace.org)                |
+| http://www.systopia.de/                                      |
++--------------------------------------------------------------*/
+
+
+CRM.$(function($) {
+  // nothing to do yet
+});
