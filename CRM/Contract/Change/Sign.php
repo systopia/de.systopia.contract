dd9384f0ffbbf03931c07f52698205d477fbbfb0
B. Endres <endres@systopia.de>
2022-08-22 16:19:16 +0200

    Revert "[#17027] if annual diff is empty, take from current annual"
    This reverts commit 6ea2e4b5a711367f9f404e61538798c0ccff1b58.
    
    See https://projekte.systopia.de/issues/17027#note-18

--- CRM/Contract/Change/Sign.php
+++ CRM/Contract/Change/Sign.php
@@ -37,9 +37,7 @@ class CRM_Contract_Change_Sign extends CRM_Contract_Change {
   public function populateData() {
     parent::populateData();
     $contract = $this->getContract(TRUE);
-    if (empty($this->data['contract_updates.ch_annual_diff'])) {
-      $this->data['contract_updates.ch_annual_diff'] = CRM_Utils_Array::value('membership_payment.membership_annual', $contract, '');
-    }
+    $this->data['contract_updates.ch_annual_diff'] = CRM_Utils_Array::value('membership_payment.membership_annual', $contract, '');
   }
 
 
