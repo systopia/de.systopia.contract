0484409701ae5ce21b5ffa02cb4faa3970aadb75
B. Endres <endres@systopia.de>
2022-08-23 15:36:27 +0200

    updated l10n

--- resources/extract_json_strings.sh
+++ resources/extract_json_strings.sh
@@ -3,7 +3,7 @@
 
 # generate temporary php file for the labels of the custom data structures
 echo '<?php\n function l10n() {' > l10n.php
-cat resources/*.json | grep '"title":' | sed 's/"title": /ts(/' | sed 's/",/");/' >> l10n.php
-cat resources/*.json | grep '"label":' | sed 's/"label": /ts(/' | sed 's/",/");/' >> l10n.php
+cat ../resources/*.json | grep '"title":' | sed 's/"title": /ts(/' | sed 's/",/");/' >> l10n.php
+cat ../resources/*.json | grep '"label":' | sed 's/"label": /ts(/' | sed 's/",/");/' >> l10n.php
 echo '}' >> l10n.php
 
