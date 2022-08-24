f7d7adb9da9bdba843f32a60ced5654c80b237ba
B. Endres <endres@systopia.de>
2022-08-24 10:52:20 +0200

    [#18017] enabled upgrages on pending contracts, tests pending

--- CRM/Contract/Change/Upgrade.php
+++ CRM/Contract/Change/Upgrade.php
@@ -180,7 +180,8 @@ class CRM_Contract_Change_Upgrade extends CRM_Contract_Change {
    * @return array list of membership status names
    */
   public static function getStartStatusList() {
-    return ['Grace', 'Current'];
+    //return ['Grace', 'Current'];
+    return ['Grace', 'Current', 'Pending'];
   }
 
   /**
