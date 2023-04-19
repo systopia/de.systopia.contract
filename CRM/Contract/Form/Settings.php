d58fcc69211ccc5b367f49636732680e18f7101d
B. Endres <endres@systopia.de>
2023-04-19 14:01:16 +0200

    [#76] added configuration option for automatic adjustment

--- CRM/Contract/Form/Settings.php
+++ CRM/Contract/Form/Settings.php
@@ -4,7 +4,27 @@ use CRM_Contract_ExtensionUtil as E;
 
 class CRM_Contract_Form_Settings extends CRM_Core_Form{
   function buildQuickForm(){
-    $this->addEntityRef('contract_modification_reviewers', E::ts('Contract modification reviewers'), ['multiple' => 'multiple']);
+    $this->addEntityRef(
+      'contract_modification_reviewers',
+      E::ts('Contract modification reviewers'),
+      ['multiple' => 'multiple']);
+
+    $this->add(
+      'select',
+      'date_adjustment',
+      E::ts('Adjust Incoming Date'),
+      [
+        '' => E::ts("don't"),
+        '1 hour' => E::ts("up to 1 hour"),
+        '2 hours' => E::ts("up to 2 hours"),
+        '6 hour' => E::ts("up to 6 hours"),
+        '12 hour' => E::ts("up to 12 hours"),
+        '1 day' => E::ts("up to 1 day"),
+        '2 day' => E::ts("up to 2 days"),
+        'always' => E::ts("always"),
+      ]
+    );
+
     $this->addButtons([
       array('type' => 'cancel', 'name' => E::ts('Back')),
       array('type' => 'submit', 'name' => E::ts('Save'))
@@ -13,20 +33,54 @@ class CRM_Contract_Form_Settings extends CRM_Core_Form{
   }
 
   function setDefaults($defaultValues = null, $filter = null){
-    $defaults = [
-      'contract_modification_reviewers' => civicrm_api3('Setting', 'GetValue', [
-        'name' => 'contract_modification_reviewers',
-        'group' => 'Contract preferences'
-      ]),
-    ];
-    parent::setDefaults($defaults);
+    parent::setDefaults([
+      'contract_modification_reviewers' => Civi::settings()->get('contract_modification_reviewers'),
+      'date_adjustment' => Civi::settings()->get('date_adjustment'),
+    ]);
   }
 
 
 
   function postProcess(){
     $submitted = $this->exportValues();
-    civicrm_api3('Setting', 'Create', ['contract_modification_reviewers' => $submitted['contract_modification_reviewers']]);
-    CRM_Core_Session::setStatus( E::ts('Contract settings updated.'), null, 'success');
+    Civi::settings()->set('contract_modification_reviewers', $submitted['contract_modification_reviewers']);
+    Civi::settings()->set('date_adjustment', $submitted['date_adjustment']);
+    CRM_Core_Session::setStatus( E::ts('Contract settings updated.'), E::ts("Success"), 'success');
+  }
+
+  /**
+   * Set the given execution time to "today"
+   *
+   * @param string $datetime
+   * @return string adjusted datetime
+   */
+  public static function adjustRequestedExecutionTime($datetime)
+  {
+    // first check, if the date is before midnight today:
+    if ($datetime < strtotime('today')) {
+      // this would cause an error, so let's see if it's in the configured range to adjust
+      $date_offset_cutoff = Civi::settings()->get('date_adjustment');
+
+      // cases for the different settings options
+      switch ($date_offset_cutoff) {
+        case 'always':
+          return strtotime('today');
+
+        case null:
+        case '':
+          // do nothing
+          return $datetime;
+
+        default:
+          // the date_offset_cutoff should be a relative time term
+          if ($datetime >= strtotime("now - {$date_offset_cutoff}")) {
+            // this is within the specified range: adjust and return
+            return strtotime('today');
+          } else {
+            // do nothing
+            return $datetime;
+          }
+      }
+    }
   }
 }
--- templates/CRM/Contract/Form/Settings.tpl
+++ templates/CRM/Contract/Form/Settings.tpl
@@ -1,6 +1,6 @@
 {*-------------------------------------------------------------+
 | SYSTOPIA Contract Extension                                  |
-| Copyright (C) 2017-2019 SYSTOPIA                             |
+| Copyright (C) 2017-2023 SYSTOPIA                             |
 | Author: M. McAndrew (michaelmcandrew@thirdsectordesign.org)  |
 | B. Endres (endres -at- systopia.de)                          |
 | http://www.systopia.de/                                      |
@@ -12,6 +12,12 @@
     <div class="crm-section payment-modify">
       <div class="label">{$form.contract_modification_reviewers.label}</div>
       <div class="content">{$form.contract_modification_reviewers.html}</div>
+      <div class="clear"></div>
+    </div>
+
+    <div class="crm-section payment-modify">
+      <div class="label">{$form.date_adjustment.label}&nbsp;{help id="id-date-adjustment" title=$form.date_adjustment.label}</div>
+      <div class="content">{$form.date_adjustment.html}</div>
       <div class="clear"></div>
     </div>
 
