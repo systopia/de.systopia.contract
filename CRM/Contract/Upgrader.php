abfa7190073c808339e29af0564bff19b984ee83
b.endres <endres@systopia.de>
2024-08-05 21:42:16 +0200

    Civix upgrade, permission definitions

--- CRM/Contract/Upgrader.php
+++ CRM/Contract/Upgrader.php
@@ -13,7 +13,7 @@ use CRM_Contract_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Contract_Upgrader extends CRM_Contract_Upgrader_Base {
+class CRM_Contract_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
