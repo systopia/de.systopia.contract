68e736abcb78e3800e28c770c558d7db700fdf20
Jens Schuppe <schuppe@systopia.de>
2026-04-16 11:22:10 +0200

    Set version to 2.0.0-beta10

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.contract',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '91017ae6a7741d9c98908211eee8c6b6d7074301',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '2.0.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.contract' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '91017ae6a7741d9c98908211eee8c6b6d7074301',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '2.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
