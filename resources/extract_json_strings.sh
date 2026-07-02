6c4708803fd453432645cb78928a6d3133b521b9
Jens Schuppe <schuppe@systopia.de>
2026-07-02 14:36:52 +0200

    Remove unnecessary script

--- resources/extract_json_strings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# regenerates a (dead) php file with all the strings in json custom data to be translated 
-
-# generate temporary php file for the labels of the custom data structures
-echo '<?php\n function l10n() {' > l10n.php
-cat ../resources/*.json | grep '"title":' | sed 's/"title": /ts(/' | sed 's/",/");/' >> l10n.php
-cat ../resources/*.json | grep '"label":' | sed 's/"label": /ts(/' | sed 's/",/");/' >> l10n.php
-echo '}' >> l10n.php
-
