23f4eaadd1f3e0b92403665d5871403e58f5d92f
Jens Schuppe <schuppe@systopia.de>
2026-07-02 12:51:32 +0200

    interim install order for mixed use of CustomData-Helper and Managed Entities
    CustomData-Helper based creation of custom fields depends on option groups being present, while the latter are being created using Managed Entities, which aren't being reconciled before CustomData-Helper tries to create custom fields.
    This change can be removed once custom fields are being created as Managed Entities themselves.

--- CRM/Contract/Upgrader.php
+++ CRM/Contract/Upgrader.php
@@ -18,8 +18,7 @@ use Civi\Api4\OptionValue;
  */
 class CRM_Contract_Upgrader extends CRM_Extension_Upgrader_Base {
 
-  public function enable(): void {
-    require_once 'CRM/Contract/CustomData.php';
+  public function postInstall(): void {
     $customData = new CRM_Contract_CustomData(E::LONG_NAME);
     $customData->syncCustomGroup(E::path('resources/custom_group_contract_cancellation.json'));
     $customData->syncCustomGroup(E::path('resources/custom_group_contract_updates.json'));
@@ -27,6 +26,10 @@ class CRM_Contract_Upgrader extends CRM_Extension_Upgrader_Base {
     $customData->syncCustomGroup(E::path('resources/custom_group_membership_general.json'));
     $customData->syncEntities(E::path('resources/entities_membership_status.json'));
     $this->ensureNoPaymentRequiredPaymentInstrument();
+  }
+
+  public function enable(): void {
+    $this->postInstall();
   }
 
   /**
